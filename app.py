d22f71629703c3a455bb6fa8a62b599c75528161
seoyeon0201 <psy0201123@gmail.com>
2024-10-10 00:04:11 +0900

    feat: add shutdown server

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+import threading
+import time
+import os
+import signal
 
 app = Flask(__name__)
 
@@ -6,5 +10,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+def shutdown_server():
+    time.sleep(60)  # 60초 대기
+    # 프로세스 ID를 가져와 서버 종료
+    os.kill(os.getpid(), signal.SIGINT)
+
 if __name__ == "__main__":
+    threading.Thread(target=shutdown_server).start()
     app.run(host='0.0.0.0', port=5000)
